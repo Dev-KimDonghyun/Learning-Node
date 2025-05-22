6d7a7a1525b48cf5331d166d39911d1d95b7c780
Dev-KimDonghyun <hyungus723@gmail.com>
2025-05-22 09:25:08 +0900

    study: mongoose를 이용하여 mongoDB Atlas 연결

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const express = require("express");
+import express from "express";
+import mongoose from "mongoose";
+import MONGOURI from "./mongoUri.js";
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello Node Express");
 });
+
+const mongoConnect = async() => {
+  try {
+    await mongoose.connect(MONGOURI);
+    console.log("Connected to MongoDb");
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+mongoConnect();
 
 app.listen(port, () => {
   console.log(`Server is Running on port ${port}`);
